acac2bdfa7676debb75ed6db0c32f4a112176d33
Bo <zboyang@umich.edu>
2021-04-05 20:42:23 -0400

    Added basic scene objects and player movement

---
+++ Assets/Scripts/Controllers/Controller_Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D ref_self_rbody = null;
+
+    private float move_x = 0f;
+    private float move_y = 0f;
+
+    private void Update()
+    {
+        move_x = Input.GetAxis("Horizontal");
+        move_y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        ref_self_rbody.AddForce(new Vector2(move_x, move_y) * Manager_Main.Instance.PLAYER_MOVE_SPEED * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Managers/Manager_Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_Main : MonoBehaviour
+{
+    public static Manager_Main Instance = null;
+
+    /* Parameters */
+
+    // Player
+    public float PLAYER_MOVE_SPEED = 0f;
+
+    // Dot
+
+    // Misc
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
